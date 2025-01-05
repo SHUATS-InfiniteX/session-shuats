037d1440aedb63170850e9b9795977179838e56e
Agamya Samuel <github@agamya.dev>
2025-01-05 21:43:21 +0530

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -5,7 +5,11 @@ async function clickHandler() {
 	const data = await fetch("https://jsonplaceholder.typicode.com/posts/1")
 	const dataToJson = await data.json()
 	// console.log(dataToJson)
-	container.innerHTML = JSON.stringify(dataToJson)
+	container.innerHTML = `<p>Title: ${dataToJson.title}</p>
+	<p>Body: ${dataToJson.body}</p>
+	<p>UserId: ${dataToJson.userId}</p>
+	<p>Id: ${dataToJson.id}</p>
+	`
 }
 
 function mouseOutHandler() {
